92c59ff6b8abb427d8865cbee2f104b1d5f8b460
gleung93 <gleung93@gmail.com>
2016-10-29 18:32:16 -0600

    try grunt sass again. It doesn't make sense to need ruby for this

--- gruntfile.js
+++ gruntfile.js
@@ -69,7 +69,7 @@ module.exports = function(grunt) {
   });
 
   grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-sass');
   grunt.loadNpmTasks('grunt-contrib-pug');
   grunt.loadNpmTasks('grunt-contrib-connect');
   grunt.loadNpmTasks('grunt-contrib-compress');
