816de515eb9dc0ae9f7de4d54227799d1a8d7d32
Taylor Griggs <tayng93@gmail.com>
2017-02-18 15:12:22 -0700

    fixed typekit

--- gruntfile.js
+++ gruntfile.js
@@ -71,7 +71,7 @@ module.exports = function(grunt) {
       server: {
         options: {
           livereload: true,
-          port: 8000,
+          port: 8080,
           base: 'dist'
         }
       }
